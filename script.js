caa3c02e4c86e2ad5591dc7d756cc389bcac3d71
MartyLake <matthieu@talbot.audio>
2018-12-16 18:48:45 +0100

    Add pretty variable to differentiate folder paths to display

--- script.js
+++ script.js
@@ -8,21 +8,27 @@ function createTable() {
             {folder:"p3", filename:"3", pretty:"Pads"},
             {folder:"p4", filename:"4", pretty:"Leads"},
         ];
-    const variants = ["C", "D"];
+    const variants = 
+        [
+            {folder:"C", pretty:"A"},
+            {folder:"D", pretty:"B"},
+        ];
 
     instrumentNamesToFolderName.forEach(function(i) {
         const folderBase = "assets/" + i.folder;
         const filenameBase = i.filename;
+        const pretty = i.pretty;
 
         var row = document.createElement('tr');
         variants.forEach(function(i) {
             const folderBase2 = folderBase + "/" + i + "/"
-            const variantName = i
+            const variantName = i.pretty
+            const variantFolder = i.folder
 
             for (j = 1; j < 4; j++) {
-                var filename = folderBase2 + filenameBase + "_" + j + variantName + ".wav"
+                var filename = folderBase2 + filenameBase + "_" + j + variantFolder + ".wav"
 
-                const cellData = filename;
+                const cellData = pretty + " " + variantName + j;
                 var cell = document.createElement('td');
                 cell.setAttribute("id", cellData);
                 cell.appendChild(document.createTextNode(cellData));
